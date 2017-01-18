724fb3e9fd84e8ec95960f2eb73a6d8595e6804e
jaclynjessup <jaclynjmoore@gmail.com>
2017-01-18 10:53:36 -0500

    feature & model tests for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true,
+  format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
+  uniqueness: { case_sensitive: false }
+  validates :password, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  root "categories#index"
   devise_for :users
-  root "home#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
