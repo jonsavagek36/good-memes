e79851f59d6e9df74142d0bba1412aa7df6c347e
jaclynjessup <jaclynjmoore@gmail.com>
2017-01-19 17:02:02 -0500

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118185448) do
+ActiveRecord::Schema.define(version: 20170119215825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20170118185448) do
     t.string   "image_url",  null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "memes", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.string   "img_url",     null: false
+    t.integer  "category_id", null: false
+    t.integer  "user_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_memes_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_memes_on_user_id", using: :btree
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -46,8 +57,22 @@ ActiveRecord::Schema.define(version: 20170118185448) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "review_id",              null: false
+    t.integer  "user_id",                null: false
+    t.integer  "upvote",     default: 0
+    t.integer  "downvote",   default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["review_id", "user_id"], name: "index_votes_on_review_id_and_user_id", unique: true, using: :btree
+    t.index ["review_id"], name: "index_votes_on_review_id", using: :btree
+    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
   end
 
 end
