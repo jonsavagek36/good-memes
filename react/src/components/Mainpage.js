d6c64916488aba7e9f79764ad29ac1fd6936713f
egiblin <egiblin88@gmail.com>
2017-01-19 16:16:28 -0500

    Added React, API, Styling on Main Page

--- react/src/components/Mainpage.js
+++ react/src/components/Mainpage.js
@@ -8,16 +8,16 @@ class Mainpage extends Component {
 
 
   render() {
+    console.log(this.props.memes[0]);
     return (
       <div>
         <h1>HOTT MEMES</h1>
-        <ul>
+        <ul id="main-page">
           {this.props.memes.map(function(meme){
-            return <li><a href='/categories'><img src={meme.url} /></a></li>;
+            return <li><a href={"categories/" + meme.id}><img id="main-list-items" src={meme.url} /></a></li>;
           })}
         </ul>
-      </div>
-
+    </div>
     );
   }
 }
