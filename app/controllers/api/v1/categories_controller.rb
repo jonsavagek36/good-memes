061d10cbf2408f12f967d5229432999b81351978
egiblin <egiblin88@gmail.com>
2017-01-23 09:52:52 -0500

    Refactored React

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::CategoriesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def index
+    @categories = Category.all
+    render json: @categories
+  end
+
+ #  def create
+ #    data = JSON.parse(request.body.read)
+ #   @category = Category.new(name: data["name"], image_url: data["image_url"])
+ #   if @category.save!
+ #     @categories = Category.all
+ #     @categories.order(:id)
+ #     render json: @categories
+ #   else
+ #     render json: {message: "Did not work"}, status: 404
+ #   end
+ # end
+end
