a679387d41d6595453d8892df96ef37b1620a647
egiblin <egiblin88@gmail.com>
2017-01-24 15:42:22 -0500

    run pending migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123190805) do
-
+ActiveRecord::Schema.define(version: 20170124152246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +60,6 @@ ActiveRecord::Schema.define(version: 20170123190805) do
     t.string   "username"
     t.boolean  "admin",                  default: false
     t.string   "avatar"
-
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
