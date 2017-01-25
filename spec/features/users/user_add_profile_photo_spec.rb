5df45a706afa09867b88b4b29a9a76e91474d672
jaclynjessup <jaclynjmoore@gmail.com>
2017-01-25 14:47:43 -0500

    readmefile

---
+++ spec/features/users/user_add_profile_photo_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+feature "profile photo" do
+  scenario "new user sees avatar upload option at edit profile page" do
+    sharona = User.create(email: "my@sharona.com", username: "sharona", password: "123456", admin: false)
+
+    visit root_path
+    click_link "Login"
+    fill_in 'Email', with: sharona.email
+    fill_in 'Password', with: sharona.password
+    click_button "Log in"
+    click_link "Edit profile"
+    expect(page).to have_button("Update")
+    expect(page).to have_content("Avatar")
+  end
+
+  scenario "user upload an avatar" do
+    sharona = User.create(email: "my@sharona.com", username: "sharona", password: "123456", admin: false)
+
+    visit root_path
+    click_link "Login"
+    fill_in 'Email', with: sharona.email
+    fill_in 'Password', with: sharona.password
+    click_button "Log in"
+    click_link "Edit profile"
+    attach_file "Choose File", "#{Rails.root}/spec/support/images/twitter-egg-icon.jpg"
+    click_button "Update"
+    visit root_path
+    expect(page).to have_content(sharona.email)
+    expect(page).to have_xpath("//img")
+  end
+
+  scenario "user edits an avatar" do
+    sharona = User.create(email: "my@sharona.com", username: "sharona", password: "123456", admin: false, avatar: "")
+
+    visit root_path
+    click_link "Login"
+    fill_in 'Email', with: sharona.email
+    fill_in 'Password', with: sharona.password
+    click_button "Log in"
+    click_link "Edit profile"
+  end
+end
