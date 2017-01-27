e7c3daebb887f7cb972ab157aefe7b39833cd0e2
jaclynjessup <jaclynjmoore@gmail.com>
2017-01-27 10:45:14 -0500

    added sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# SendGrid E-Mail Set Up
+ActionMailer::Base.smtp_settings = {
+  :user_name => SENDGRID_NAME,
+  :password => SENDGRID_PASSWORD,
+  :domain => 'good-memes.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
